5c6c102a593cc2ea73225239843307c1b3b3d333
Andeilson Ferreira <agf@Mac-Pro-de-Andeilson.local>
2022-02-03 12:03:55 -0300

    fix load dashboard screen

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -4,9 +4,12 @@ import React, {
     useState
 } from "react";
 
+import { ActivityIndicator } from 'react-native'
+
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { useFocusEffect } from '@react-navigation/native'
+import { useFocusEffect } from '@react-navigation/native';
+import { useTheme } from 'styled-components'
 
 import { HighlightCard } from "../../components/HighlightCard";
 
@@ -31,24 +34,47 @@ import {
      Transactions,
      Title,
      TransactionList,
-     LogoutButton
+     LogoutButton,
+     LoadContainer
  } from './styles';
 
  export interface DataListProps extends TransactionsCardProps {
      id: string;
  }
 
-export function Dashboard(){
+ interface HighlightProps {
+     amount: string;
+ }
 
-    const [data, setData] = useState<DataListProps[]>([]);
+ interface HighlightData {
+     entries: HighlightProps,
+     expensives: HighlightProps
+     total: HighlightProps
+ }
+
+export function Dashboard(){
+    const [isLoading, setIsLoading] = useState(true);
+    const [transactions, setTransactions] = useState<DataListProps[]>([]);
+    const [highlightData, setHighlightData] = useState<HighlightData>({} as HighlightData);
+
+    const theme = useTheme();
 
     async function loadTransactions() {
         const dataKey = '@gofinances:transactions';
         const response = await AsyncStorage.getItem(dataKey);
         const transactions = response ? JSON.parse(response) : []
 
+        let entriesTotal = 0;
+        let expensiveTotal = 0;
+
         const transactionsFormatted: DataListProps[] = transactions
         .map((item: DataListProps) => {
+
+            if (item.type === 'positive') {
+                entriesTotal += Number(item.amount)
+            } else {
+                expensiveTotal += Number(item.amount)  
+            }
 
             const amount = Number(item.amount)
             .toLocaleString('pt-BR', {
@@ -73,17 +99,34 @@ export function Dashboard(){
 
     });
 
-    setData(transactionsFormatted);
+    setTransactions(transactionsFormatted);
+    const total = entriesTotal - expensiveTotal;
 
+    setHighlightData({
+        entries: {
+            amount: entriesTotal.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+            })
+        },
+        expensives: {
+            amount: expensiveTotal.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+            })
+        },
+        total: {
+            amount: total.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+            })}
+        });
+
+        setIsLoading(false);
     }
 
     useEffect(() => {
         loadTransactions();
-
-        // Limpar transações async
-        
-        // const dataKey = '@gofinances:transactions';
-        // AsyncStorage.removeItem(dataKey);
 
     }, [])
 
@@ -93,6 +136,15 @@ export function Dashboard(){
 
     return (
         <Container>
+            {
+                isLoading ?
+                <LoadContainer>
+                    <ActivityIndicator
+                    color={theme.colors.primary}
+                    size="large"
+                    />
+                </LoadContainer> : 
+            <>
             <Header>
                 <UserWrapper>
                     <UserInfo>
@@ -110,25 +162,43 @@ export function Dashboard(){
                 </UserWrapper>
             </Header>
        
-         <HighlightCards>
-                <HighlightCard type="up" title="Entrada" amount="R$ 17.400,00" lastTransaction="Última entrada dia 13 de abril"/>
-                <HighlightCard type="down" title="Saídas" amount="R$ 1.259,00" lastTransaction="Última saída dia 03 de abril" />
-                <HighlightCard type="total"  title="Total" amount="R$ 16.141,00" lastTransaction="01 à 16 de abril" />
-         </HighlightCards>
+                <HighlightCards>
+                        <HighlightCard type="up"
+                        title="Entrada"
+                        amount={highlightData.entries.amount}
+                        lastTransaction="Última entrada dia 13 de abril"
+                        />
+
+                        <HighlightCard
+                        type="down"
+                        title="Saídas"
+                        amount={highlightData.expensives.amount}
+                        lastTransaction="Última saída dia 03 de abril"
+                        />
+
+                        <HighlightCard type="total"
+                        title="Total"
+                        amount={highlightData.total.amount}
+                        lastTransaction="01 à 16 de abril"
+                        />
+                        
+                </HighlightCards>
  
-        <Transactions>
-            <Title>Listagem</Title>
+                <Transactions>
+                    <Title>Listagem</Title>
 
-            <TransactionList
-            
-                data={data}
-                keyExtractor={item => item.id}
-                renderItem={({ item }) => <TransactionCard data={item} />}
-            
-            />
+                    <TransactionList
+                    
+                        data={transactions}
+                        keyExtractor={item => item.id}
+                        renderItem={({ item }) => <TransactionCard data={item} />}
+                    
+                    />
 
-        </Transactions>
-        
+                </Transactions>
+
+                </>
+            }
 
         </Container>
     );
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -122,3 +122,9 @@ export const  TransactionList = styled(
         paddingBottom: getBottomSpace()
     }
 })``;
+
+export const LoadContainer = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
